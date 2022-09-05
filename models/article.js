0d13c31102161d08f6fbb2f68b943d90361108ad
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-09-05 18:31:09 +0200

    convert to HTML and sanitize markdown input

--- models/article.js
+++ models/article.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const marked = require('marked');
 const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new Schema({
     title: {
@@ -23,6 +26,10 @@ const articleSchema = new Schema({
         type: String,
         requried: true,
         unique: true,
+    },
+    sanitizedHtml: {
+        type: String,
+        required: true,
     }
 });
 
@@ -30,6 +37,11 @@ const articleSchema = new Schema({
 articleSchema.pre('validate', function(next) {
     if (this.title) {
         this.slug = slugify(this.title, { lower: true, strict: true});
+    }
+
+    // converts markdown to HTML and then sanitize it
+    if (this.markdown) {
+        this.sanitizedHtml = dompurify.sanitize(marked.parse(this.markdown));
     }
 
     next();
